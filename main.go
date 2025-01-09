f5c7352eee342ab1195b65c466dbe92be941a31e
hansfigo <hambasahaya0303@gmail.com>
2025-01-09 14:28:55 +0700

    feat : add main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Simple HTML</title>
+		</head>
+		<body>
+			<h1>Tech Talk AMCC #1</h1>
+		</body>
+		</html>
+		`
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, html)
+	})
+
+	fmt.Println("Server is running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
