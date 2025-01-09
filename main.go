a8da884a7003bc356c0b67c4fd08ba106cc1bff8
hansfigo <hambasahaya0303@gmail.com>
2025-01-09 14:46:25 +0700

    feat:add about page

--- main.go
+++ main.go
@@ -24,6 +24,25 @@ func main() {
 		fmt.Fprint(w, html)
 	})
 
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		html := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Simple HTML</title>
+		</head>
+		<body>
+			<h1>Tech Talk AMCC #1</h1>
+			<p>This is about Page</p>
+		</body>
+		</html>
+		`
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, html)
+	})
+
 	fmt.Println("Server is running at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
